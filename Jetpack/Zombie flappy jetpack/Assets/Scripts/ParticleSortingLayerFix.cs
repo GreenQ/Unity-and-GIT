7f4864c22855126d73b498b1ca59df255b515da0
AsCross <tarasdudyak@gmail.com>
2015-07-20 20:32:30 +0300

    Mouse

---
+++ Jetpack/Zombie flappy jetpack/Assets/Scripts/ParticleSortingLayerFix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSortingLayerFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().sortingLayerName = "Player";
+		GetComponent<Renderer>().sortingOrder = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
